fe5c1b95d95ccde9eabe266af71c23e5f138f3ee
S. Michael Hussain <smhussain_1@yahoo.com>
2019-04-24 20:34:14 -0700

    first commit of initial files

---
+++ index.js
@@ -0,0 +1,37 @@
+/*
+Requirements:
+The user must be able to search for parks in one or more states.
+The user must be able to set the max number of results, with a default of 10.
+The search must trigger a call to NPS's API.
+The parks in the given state must be displayed on the page. Include at least:
+    Full name
+    Description
+    Website URL
+The user must be able to make multiple searches and see only the results for the current search.
+As a stretch goal, try adding the park's address to the results.
+*/
+
+function userSearch() {
+    // grabs the name of the park(s) in the state that the user wants to see
+
+}
+
+function maxResults() {
+    // grabs the max number of records the user specifies
+    // defaults to 10
+
+}
+
+function callApi() {
+    // exeuctes call to the API and gets the data
+}
+
+function displayResults() {
+    // displays final results to users, after clearing out previous results
+    // displays Full Name of Park, Description, URL and Address
+}
+
+$(
+
+
+);
